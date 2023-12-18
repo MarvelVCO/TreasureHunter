b6282dd0a032699bc3f677bcf83d1e619136f8cc
FirePheonix123 <muhammadt47@nycstudents.net>
2023-12-18 11:08:34 -0500

    Task 1

--- src/TreasureHunterRunner.java
+++ src/TreasureHunterRunner.java
@@ -1,5 +1,6 @@
 public class TreasureHunterRunner {
     public static void main(String[] args) {
+        System.out.println("Welcome to " + Colors.CYAN + "TREASURE HUNTER" + Colors.RESET + "!");
         TreasureHunter game = new TreasureHunter();
         game.play();
     }
